5e582b5174e46d096d2a4cf50fb2f2cdd88ea6cc
jws909 <jws090909@gmail.com>
2026-06-11 16:06:09 +0900

    리액트 useState 기본개념

--- react-study/src/App.jsx
+++ react-study/src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import Basic01 from './components/Basic01'
+// import Basic01 from './components/Basic01'
+import State01 from './components/State01'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Basic01 />
+    // <Basic01/>
+    <State01/>
   )
 }
 
