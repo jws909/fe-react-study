38879ca0aafa627419a017f7b3a1f96616b2463b
jws909 <jws090909@gmail.com>
2026-06-12 15:26:42 +0900

    리액트 배열(참조변수) state 변수 사용

--- react-study/src/App.jsx
+++ react-study/src/App.jsx
@@ -3,10 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import State03 from './components/State03'
 // import Basic01 from './components/Basic01'
 // import State01 from './components/State01'
 // import Export01 from './components/Export01'
-import State02 from './components/State02'
+// import State02 from './components/State02'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,7 +17,8 @@ function App() {
     // <Basic01/>
     // <State01/>
     // <Export01/>
-    <State02/>
+    // <State02/>
+    <State03/>
   )
 }
 
---
+++ react-study/src/components/State03.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+function State03(){
+
+    // let arr = [1,2,3,4,5];  //일반 배열 변수
+    let [arr, setArr] = useState([1,2,3,4,5]);
+
+    // let count = 10;  count = 20;
+
+    // 참조변수 (주소)
+    // {key:value, key:value}
+    // [ㅇ, ㅇ, ㅇ]
+    // {key:value, key:value, key:[]}
+    // [{}, {}, {}]
+
+    return (
+        <div>
+            <button onClick={()=>{
+                //배열에 값 추가 : push unshift splice
+                //  [1,2,3,4,5];
+                // arr.push(6); //[1,2,3,4,5, 6] [1,2,3,4,5, 6,6]
+                // console.log(arr);
+                
+                //arr = [1,2,3,4,5]
+                // arr.push(7);    //arr = [1,2,3,4,5, 7];
+                // setArr(arr);    //화면 재렌더링이 발생하지 않음
+
+                // setArr([1,2,3,4,5,7]);
+
+                // 깊은복사 -> 새로운 주소에 데이터 생성...
+                // let temp = arr; //얕은 복사
+
+                // let temp = [...arr];
+                // temp.push(7);
+                // setArr(temp);
+
+                arr.push(7);
+                setArr([...arr]);
+
+            }}>배열변경버튼</button>
+
+            {
+                arr.map((val)=>{
+                    return <p>{val}</p>;
+                })
+            }
+        </div>
+    );
+}
+
+export default State03;
