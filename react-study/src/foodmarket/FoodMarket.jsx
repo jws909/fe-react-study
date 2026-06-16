9b3e5f6c3c18e9a83e6b12205c278e4b7358605b
jws909 <jws090909@gmail.com>
2026-06-16 16:27:06 +0900

    리액트 이미지 사용, bootstrap 컴포넌트 사용

--- react-study/src/foodmarket/FoodMarket.jsx
+++ react-study/src/foodmarket/FoodMarket.jsx
@@ -1,7 +1,108 @@
 import './FoodMarket.css';
+// import Button from 'react-bootstrap/Button';
+// import Spinner from 'react-bootstrap/Spinner';
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+import { Container, Nav, Navbar, Row, Col, Card, Button } from 'react-bootstrap';
+import banner_bg from './img/banner_bg.jpg';
+import food1 from './img/food1.jpg';
+import food2 from './img/food2.jpg';
+import food3 from './img/food3.jpg';
 
-function FoodMarket(){
+function FoodMarket() {
 
+    return (
+        <div>
+
+            <Navbar bg="light" data-bs-theme="light">
+                <Container>
+                    <Navbar.Brand href="#home">FoodMarket</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+
+            {/*
+                이미지 사용
+                
+                상대경로 사용 XXX
+                <img src="../../image.menu.png"/> XXX
+
+
+                react 컴포넌트에서 이미지 사용시, import -> 사용
+                <img src="https://...외부url"/>
+                <img src={import한이미지}/>
+            */}
+
+            {/* <img src={banner_bg} /> */}
+            {/* <div className='main-bg' style={{backgroundImage:'url('+ banner_bg +')'}}></div> */}
+            <div className='main-bg'></div>
+
+            {/*
+                bootstrap 한줄 기준 : 12
+            */}
+            <Container>
+                <Row>
+                    <Col md={4} sm={2}>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src={food1} />
+                            <Card.Body>
+                                <Card.Title>카드 타이틀 div태그</Card.Title>
+                                <Card.Text>
+                                    내부 내용쓰는 곳. p태그 인식
+                                </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col md={4} sm={2}>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src={food2} />
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col md={4} sm={2}>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src={food3} />
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+
+
+            {/* <Spinner animation="border" role="status">
+                <span className="visually-hidden">Loading...</span>
+            </Spinner> */}
+            {/* <Button variant="primary">Primary</Button>
+            <Button variant="secondary">Secondary</Button>
+            <Button variant="success">Success</Button>
+            <Button variant="warning">Warning</Button>
+            <Button variant="danger">Danger</Button>
+            <Button variant="info">Info</Button>
+            <Button variant="light">Light</Button>
+            <Button variant="dark">Dark</Button>
+            <Button variant="link">Link</Button> */}
+        </div>
+    );
 }
 
 export default FoodMarket;
