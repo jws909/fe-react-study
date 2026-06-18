6ab9dbec8160d319f36be29ed721ec30b45f73b3
jws909 <jws090909@gmail.com>
2026-06-18 16:53:27 +0900

    foodsData 총 9개 데이터 버전

--- react-study/src/foodmarket/data/foodsData.js
+++ react-study/src/foodmarket/data/foodsData.js
@@ -24,6 +24,55 @@ let foodsData = [
         price: 11000,
         imgPath: 'images/food3.jpg',
         stockCount: 0
+    },
+
+    {
+        id: "fd004",
+        title: "팬케이크",
+        content: "메이플 시럽 잔뜩",
+        price: 8000,
+        imgPath: "images/food4.jpg",
+    },
+    {
+        id: "fd005",
+        title: "이름 모를 음식",
+        content: "보긴 많이 봤는데",
+        price: 13000,
+        imgPath: "images/food5.jpg",
+        stockCount: 3
+    },
+    {
+        id: "fd006",
+        title: "피자",
+        content: "1인에 한판 피자",
+        price: 15000,
+        imgPath: "images/food6.jpg",
+        stockCount: 5
+    },
+
+    {
+        id: "fd007",
+        title: "연어 스테이크",
+        content: "고양이도 탐내는",
+        price: 9000,
+        imgPath: "images/food7.jpg",
+        stockCount: 7
+    },
+    {
+        id: "fd008",
+        title: "새우구이",
+        content: "살짝 탄맛",
+        price: 11000,
+        imgPath: "images/food8.jpg",
+        stockCount: 9
+    },
+    {
+        id: "fd009",
+        title: "와플",
+        content: "크림이 없어요",
+        price: 2500,
+        imgPath: "images/food9.jpg",
+        stockCount: 1
     }
 ];
 
